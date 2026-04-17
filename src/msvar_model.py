f80a70373d375fe44c7de77ecfc94e7523a9f85c
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:16:09 +0700

    Add MS-VAR model module

---
+++ src/msvar_model.py
@@ -0,0 +1,16 @@
+"""
+Estimate hidden regimes using a Markov-Switching VAR.
+"""
+
+
+class MSVARRegimeModel:
+    """Markov-Switching VAR for macro regime detection."""
+
+    def fit(self, X):
+        pass
+
+    def predict_states(self, X):
+        pass
+
+    def predict_probabilities(self, X):
+        pass
