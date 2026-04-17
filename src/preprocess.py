fcf97a7c9c001cc9356aa6889e55cb9c1fd8a278
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:14:33 +0700

    Add preprocessing module

---
+++ src/preprocess.py
@@ -0,0 +1,18 @@
+"""
+Preprocess monthly macroeconomic and asset data into a model-ready dataset.
+"""
+
+
+def align_dates(macro_df, asset_df):
+    """Align macro and asset data on a common monthly calendar."""
+    pass
+
+
+def transform_variables(df, transform_spec=None):
+    """Apply transformations to create model-ready features."""
+    pass
+
+
+def standardize_features(df):
+    """Standardize features where appropriate."""
+    pass
