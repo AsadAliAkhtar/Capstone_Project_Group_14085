155e9260d5ae1fcef58eafb28c80c0bac4e7418b
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:18:33 +0700

    Add performance metrics module

---
+++ src/metrics.py
@@ -0,0 +1,19 @@
+"""
+Compute performance metrics for tactical allocation strategies.
+"""
+
+
+def annualized_return(returns):
+    pass
+
+
+def annualized_volatility(returns):
+    pass
+
+
+def sharpe_ratio(returns, risk_free_rate=0.0):
+    pass
+
+
+def max_drawdown(returns):
+    pass
