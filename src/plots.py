ce490e9a525de6308837d4df9bce7c6c47f9b59d
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:18:58 +0700

    Add plotting module

---
+++ src/plots.py
@@ -0,0 +1,13 @@
+"""
+Generate figures for regime outputs and backtest results.
+"""
+
+
+def plot_cumulative_returns(results):
+    """Plot cumulative portfolio returns."""
+    pass
+
+
+def plot_regime_probabilities(probabilities):
+    """Plot regime probabilities over time."""
+    pass
