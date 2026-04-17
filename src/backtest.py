dc2d3a54cf69e8a6675ea8d4b78e4bc582e9ab78
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:17:20 +0700

    Add backtesting module

---
+++ src/backtest.py
@@ -0,0 +1,8 @@
+"""
+Run monthly backtests for regime-based allocation strategies.
+"""
+
+
+def run_backtest(weights, returns, transaction_cost_bps=0):
+    """Run backtest with transaction costs."""
+    pass
