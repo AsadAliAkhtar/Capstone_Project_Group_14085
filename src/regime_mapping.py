908a37f8359b304a21efd70522091773ef1e38a0
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:16:46 +0700

    Add regime mapping module

---
+++ src/regime_mapping.py
@@ -0,0 +1,8 @@
+"""
+Map inferred regimes to asset allocation signals.
+"""
+
+
+def map_regime_to_weights(regime_output, mapping_spec):
+    """Convert regime output into portfolio weights."""
+    pass
