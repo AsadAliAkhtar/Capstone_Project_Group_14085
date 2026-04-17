82ace95812a42412d44e82a73b0b2d1d1938ae90
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:17:47 +0700

    Add benchmark module

---
+++ src/benchmarks.py
@@ -0,0 +1,13 @@
+"""
+Construct benchmark portfolios for comparison.
+"""
+
+
+def build_equal_weight_benchmark(returns):
+    """Construct equal-weight benchmark."""
+    pass
+
+
+def build_sixty_forty_benchmark(returns):
+    """Construct 60/40 benchmark."""
+    pass
