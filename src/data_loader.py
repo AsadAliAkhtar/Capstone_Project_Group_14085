750cff4a1dec9c44478f34b50e660a75bf80ed38
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:12:54 +0700

    Add data loader module

---
+++ src/data_loader.py
@@ -0,0 +1,13 @@
+"""
+Load and organize macroeconomic and asset data for the capstone project.
+"""
+
+
+def load_macro_data(path):
+    """Load monthly macroeconomic data."""
+    pass
+
+
+def load_asset_data(path):
+    """Load monthly asset data."""
+    pass
