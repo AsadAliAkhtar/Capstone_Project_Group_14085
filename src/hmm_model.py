e3d5ed05a48458bc20aef5c4309acb73d2066b41
calvinbm <khang.buimanh99@gmail.com>
2026-04-17 10:15:24 +0700

    Add HMM model module

---
+++ src/hmm_model.py
@@ -0,0 +1,16 @@
+"""
+Estimate hidden regimes using a Hidden Markov Model.
+"""
+
+
+class HMMRegimeModel:
+    """Hidden Markov Model for macro regime detection."""
+
+    def fit(self, X):
+        pass
+
+    def predict_states(self, X):
+        pass
+
+    def predict_probabilities(self, X):
+        pass
